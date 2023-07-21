133f2d2c26389c767fc36e3ab0f1b8659fa02cb8
Tran Trong Hien <tronghientran18@gmail.com>
2023-07-22 02:43:17 +0700

    Demo only get previous url

--- src/main/java/com/poly/controller/GlobalControllerAdvice.java
+++ src/main/java/com/poly/controller/GlobalControllerAdvice.java
@@ -3,13 +3,20 @@ package com.poly.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.poly.service.SessionService;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
+@SessionAttributes("previousUrl")
 public class GlobalControllerAdvice {
 	@Autowired
 	SessionService session;
+
+	@Autowired
+	HttpServletRequest request;
 
 	@ModelAttribute("totalCart")
 	public Integer getTotalCart() {
@@ -20,5 +27,11 @@ public class GlobalControllerAdvice {
 	@ModelAttribute("account")
 	public Boolean getIsLogin() {
 		return session.get("account") != null ? true : false;
+	}
+
+	public String getPreviousUrlPath() {
+		String previousUrl = request.getRequestURL().toString();
+		session.set("previousUrl", previousUrl);
+		return previousUrl;
 	}
 }
