3751488f37f4852bc295a43ec3f9692ec83240de
Tran Trong Hien <tronghientran18@gmail.com>
2023-08-09 15:39:11 +0700

    fix UI - Dashboard - Admin

--- src/main/3mems-fe/src/pages/Dashboard.js
+++ src/main/3mems-fe/src/pages/Dashboard.js
@@ -1,12 +1,26 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faCog, faHome, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { Col, Row, Form, Button, ButtonGroup, Breadcrumb, InputGroup, Dropdown } from '@themesberg/react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheck,
+  faCog,
+  faHome,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  Col,
+  Row,
+  Form,
+  Button,
+  ButtonGroup,
+  Breadcrumb,
+  InputGroup,
+  Dropdown,
+} from "@themesberg/react-bootstrap";
 
 import { TransactionsTable } from "../components/Tables";
 
-const url = 'http://localhost:8080/admin/index';
-const imageUrl = 'http://localhost:8080/img/product/';
+const url = "http://localhost:8080/admin/index";
+const imageUrl = "http://localhost:8080/img/product/";
 
 export default () => {
   const [topTenOrderDetails, setTopTenOrderDetails] = useState([]);
@@ -20,49 +34,49 @@ export default () => {
   // fetchBestingSelling
   const fetchBestSellingProduct = async () => {
     try {
-      const response = await fetch(`${url}/bestSellingProduct`)
-      const data = await response.json()
-      const bestSelling = data
+      const response = await fetch(`${url}/bestSellingProduct`);
+      const data = await response.json();
+      const bestSelling = data;
 
       if (bestSelling) {
         const newbestSelling = bestSelling.map((item) => {
-          const { id, name } = item
+          const { id, name } = item;
           return {
             id: id,
             name: name,
-          }
-        })
+          };
+        });
         setBestSellingProduct(newbestSelling);
       } else {
         setBestSellingProduct([]);
       }
     } catch (error) {
-      console.log('Error: ' + error)
+      console.log("Error: " + error);
     }
-  }
+  };
 
   // ProductNewset
   const fetchProductNewest = async () => {
     try {
-      const response = await fetch(`${url}/selectProductNewest`)
-      const data = await response.json()
-      const newproductNewest = data
+      const response = await fetch(`${url}/selectProductNewest`);
+      const data = await response.json();
+      const newproductNewest = data;
       if (newproductNewest.length === 0) {
         setProductNewest(0);
       } else {
         setProductNewest(newproductNewest);
       }
     } catch (error) {
-      console.log('Error: ' + error)
+      console.log("Error: " + error);
     }
-  }
+  };
 
   // TotalRenue
   const fetchtotalRenue = async () => {
     try {
-      const response = await fetch(`${url}/totalRevenue`)
-      const data = await response.json()
-      const newTotalRenue = data
+      const response = await fetch(`${url}/totalRevenue`);
+      const data = await response.json();
+      const newTotalRenue = data;
       if (newTotalRenue === null) {
         setTotalReneue(0);
         console.log(totalReneue);
@@ -70,48 +84,48 @@ export default () => {
         setTotalReneue(newTotalRenue);
       }
     } catch (error) {
-      console.log('Error: ' + error)
+      console.log("Error: " + error);
     }
-  }
+  };
 
   // TotalProductSoldToday
   const fetchTotalProductsSoldToday = async () => {
     try {
-      const response = await fetch(`${url}/totalProductsSoldToday`)
-      const data = await response.json()
-      const newTotalProductSoldToday = data
+      const response = await fetch(`${url}/totalProductsSoldToday`);
+      const data = await response.json();
+      const newTotalProductSoldToday = data;
       setTotalProductSoldToday(newTotalProductSoldToday);
     } catch (error) {
-      console.log('Error: ' + error)
+      console.log("Error: " + error);
     }
-  }
+  };
 
   // StatusProduct
   const fetchStatusProduct = async () => {
     try {
-      const response = await fetch(`${url}/findByStatusC`)
-      const data = await response.json()
-      const newStatusProduct = data
+      const response = await fetch(`${url}/findByStatusC`);
+      const data = await response.json();
+      const newStatusProduct = data;
       if (newStatusProduct.length === 0) {
-        setStatusProduct(0)
+        setStatusProduct(0);
       }
-      setStatusProduct(newStatusProduct.length)
+      setStatusProduct(newStatusProduct.length);
     } catch (error) {
-      console.log('Error: ' + error)
+      console.log("Error: " + error);
     }
-  }
+  };
 
   // StatusProduct
   const fetchPageTopTenOrderDetails = async () => {
     try {
-      const response = await fetch(`${url}/getPageTopTenOrderDetails`)
-      const data = await response.json()
-      const newTopTenOrder = data
-      setTopTenOrderDetails(newTopTenOrder)
+      const response = await fetch(`${url}/getPageTopTenOrderDetails`);
+      const data = await response.json();
+      const newTopTenOrder = data;
+      setTopTenOrderDetails(newTopTenOrder);
     } catch (error) {
-      console.log('Error: ' + error)
+      console.log("Error: " + error);
     }
-  }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -124,89 +138,123 @@ export default () => {
           fetchtotalRenue(),
           fetchTotalProductsSoldToday(),
           fetchStatusProduct(),
-          fetchPageTopTenOrderDetails()
+          fetchPageTopTenOrderDetails(),
         ]);
 
         setIsLoading(false); // Completed fetching, isLoading is false
       } catch (error) {
-        console.log('error: ' + error);
+        console.log("error: " + error);
       }
     };
 
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <>
-      {
-        isLoading ? (
-          <h3>Loading...</h3>
-        ) : (
-          <>
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
-              <div className="d-block mb-4 mb-md-0">
-                <Breadcrumb className="d-none d-md-inline-block" listProps={{ className: "breadcrumb-dark breadcrumb-transparent" }}>
-                  <Breadcrumb.Item><FontAwesomeIcon icon={faHome} /></Breadcrumb.Item>
-                  <Breadcrumb.Item active>Trang Chủ</Breadcrumb.Item>
-                </Breadcrumb>
-                <h3>Chào mừng trở lại, <span className="fs-3 fw-bold">YourAdminName</span></h3>
-                {/* <p className="mb-0">Your web analytics dashboard template.</p> */}
-              </div>
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <>
+          <div
+            className="d-flex justify-content-between 
+              container-fluid flex-wrap 
+              flex-md-nowrap align-items-center py-4"
+          >
+            <div className="d-block mb-4 mb-md-0">
+              <Breadcrumb
+                className="d-none d-md-inline-block"
+                listProps={{
+                  className: "breadcrumb-dark breadcrumb-transparent",
+                }}
+              >
+                <Breadcrumb.Item>
+                  <FontAwesomeIcon icon={faHome} />
+                </Breadcrumb.Item>
+                <Breadcrumb.Item active>Trang Chủ</Breadcrumb.Item>
+              </Breadcrumb>
+              <h3>
+                Chào mừng trở lại,{" "}
+                <span className="fs-3 fw-bold">YourAdminName</span>
+              </h3>
+              {/* <p className="mb-0">Your web analytics dashboard template.</p> */}
+            </div>
 
-              {/* <div className="btn-toolbar mb-2 mb-md-0">
+            {/* <div className="btn-toolbar mb-2 mb-md-0">
             <ButtonGroup>
               <Button variant="outline-primary" size="sm">Share</Button>
               <Button variant="outline-primary" size="sm">Export</Button>
             </ButtonGroup>
           </div> */}
-            </div>
+          </div>
 
-            {/* statistic concept*/}
-            <div className="d-flex justify-content-between gap-3">
-              {/* Item */}
-              <div className="card text-center col-xl-3 col-lg-3 col-md-6 col-sm-12" style={{ background: "#effcef" }}>
-                <div className="card-body">
-                  <h3 className="card-title fw-bold fs-3">{productNewest}</h3>
-                  <p className="text-dark fw-semibold">Sản phẩm vừa thêm</p>
-                </div>
-              </div>
-              {/* Item */}
-              <div className="card text-center col-xl-3 col-lg-3 col-md-6 col-sm-12" style={{ background: "#e6f5f9" }}>
-                <div className="card-body">
-                  <h3 className="card-title fw-bold fs-3">{totalReneue}</h3>
-                  <p className="text-dark fw-semibold">Tổng doanh thu</p>
-                </div>
-              </div>
-              {/* Item */}
-              <div className="card text-center col-xl-3 col-lg-3 col-md-6 col-sm-12" style={{ background: "#f4f6fa" }}>
-                <div className="card-body">
-                  <h3 className="card-title fw-bold fs-3">{totalProductSoldToday}</h3>
-                  <p className="text-dark fw-semibold">Tổng sản phẩm bán được trong ngày</p>
-                </div>
-              </div>
-              {/* Item */}
-              <div className="card text-center col-xl-3 col-lg-3 col-md-6 col-sm-12" style={{ background: "#f4f6fa" }}>
-                <div className="card-body">
-                  <h3 className="card-title fw-bold fs-3">{statusProduct}</h3>
-                  <p className="text-dark fw-semibold">Tổng sản phẩm chờ xác nhận</p>
-                </div>
+          {/* statistic concept*/}
+          <div className="container-fluid d-flex justify-content-start gap-3">
+            {/* Item */}
+            <div
+              className="card text-center col-xl-3 col-lg-3 col-md-6 col-sm-12"
+              style={{ background: "#effcef" }}
+            >
+              <div className="card-body">
+                <h3 className="card-title fw-bold fs-3">{productNewest}</h3>
+                <p className="text-dark fw-semibold">Sản phẩm vừa thêm</p>
               </div>
             </div>
+            {/* Item */}
+            <div
+              className="card text-center col-xl-3 col-lg-3 col-md-6 col-sm-12"
+              style={{ background: "#e6f5f9" }}
+            >
+              <div className="card-body">
+                <h3 className="card-title fw-bold fs-3">{totalReneue}</h3>
+                <p className="text-dark fw-semibold">Tổng doanh thu</p>
+              </div>
+            </div>
+            {/* Item */}
+            <div
+              className="card text-center col-xl-3 col-lg-3 col-md-6 col-sm-12"
+              style={{ background: "#f4f6fa" }}
+            >
+              <div className="card-body">
+                <h3 className="card-title fw-bold fs-3">
+                  {totalProductSoldToday}
+                </h3>
+                <p className="text-dark fw-semibold">
+                  Tổng sản phẩm bán được trong ngày
+                </p>
+              </div>
+            </div>
+            {/* Item */}
+            <div
+              className="card text-center col-xl-3 col-lg-3 col-md-6 col-sm-12"
+              style={{ background: "#f4f6fa" }}
+            >
+              <div className="card-body">
+                <h3 className="card-title fw-bold fs-3">{statusProduct}</h3>
+                <p className="text-dark fw-semibold">
+                  Tổng sản phẩm chờ xác nhận
+                </p>
+              </div>
+            </div>
+          </div>
 
-            {/* Content Transaction */}
-            <div className="main-content mt-4">
-              <div className="row">
-                <h4>Tổng Giao dịch trong ngày: <strong>{topTenOrderDetails.length}</strong></h4>
-                {/* Item */}
-                <div className="col-8">
-                  <div className="card-full-height">
-                    <div className="card h-100">
-                      <div className="card-body">
-                        <h4 className="card-title fw-bold mb-4">
-                          <i class="fas fa-play"></i> Giao dịch
-                        </h4>
-                        <div className="row">
-                          {/* {topTenOrderDetails.map((orderDetail) => (
+          {/* Content Transaction */}
+          <div className="main-content container-fluid mt-4">
+            <div className="row">
+              <h4>
+                Tổng Giao dịch trong ngày:{" "}
+                <strong>{topTenOrderDetails.length}</strong>
+              </h4>
+              {/* Item */}
+              <div className="col-8">
+                <div className="card-full-height">
+                  <div className="card h-100">
+                    <div className="card-body">
+                      <h4 className="card-title fw-bold mb-4">
+                        <i class="fas fa-play"></i> Giao dịch
+                      </h4>
+                      <div className="row">
+                        {/* {topTenOrderDetails.map((orderDetail) => (
                         <div className="col-12 mb-4" key={orderDetail.id}>
                           <div className="d-flex justify-content-between align-items-center flex-wrap">
                             <div className="p-2 d-flex justify-content-center align-items-center" style={{ background: '#dc3545', width: '35px', height: '35px', borderRadius: '50%' }}>
@@ -226,72 +274,72 @@ export default () => {
                           <hr className="m-0 p-0" />
                         </div>
                       ))} */}
-                          <div class="table-responsive">
-                            <table class="table table-striped
+                        <div class="table-responsive">
+                          <table
+                            class="table table-striped
                         table-hover
-                        align-middle">
-                              <thead class="">
-                                <caption>Transaction</caption>
-                                <tr style={{ background: "#e6f5f9" }}>
-                                  <th>ID</th>
-                                  <th>Hình ảnh</th>
-                                  <th>Tên sản phẩm</th>
-                                  <th>Giá sản phẩm</th>
-                                  <th>Tổng tiền</th>
-                                  <th>Số lượng</th>
+                        align-middle"
+                          >
+                            <thead class="">
+                              <caption>Transaction</caption>
+                              <tr style={{ background: "#e6f5f9" }}>
+                                <th>ID</th>
+                                <th>Hình ảnh</th>
+                                <th>Tên sản phẩm</th>
+                                <th>Giá sản phẩm</th>
+                                <th>Tổng tiền</th>
+                                <th>Số lượng</th>
+                              </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+                              {topTenOrderDetails.map((o) => (
+                                <tr key={o.id}>
+                                  <td>{o.id}</td>
+                                  <td>
+                                    <img
+                                      src={imageUrl + o.product.image}
+                                      style={{ width: "60px", height: "60px" }}
+                                    />
+                                  </td>
+                                  <td>{o.product.name}</td>
+                                  <td>{o.product.price}</td>
+                                  <td>{o.price}</td>
+                                  <td>{o.quantity}</td>
                                 </tr>
-                              </thead>
-                              <tbody class="table-group-divider">
-                                {
-                                  topTenOrderDetails.map((o) => (
-                                    <tr key={o.id}>
-                                      <td>{o.id}</td>
-                                      <td>
-                                        <img src={imageUrl + o.product.image}
-                                          style={{ width: "60px", height: "60px" }} />
-                                      </td>
-                                      <td>{o.product.name}</td>
-                                      <td>{o.product.price}</td>
-                                      <td>{o.price}</td>
-                                      <td>{o.quantity}</td>
-                                    </tr>
-                                  ))
-                                }
-                              </tbody>
-                            </table>
-                          </div>
-
+                              ))}
+                            </tbody>
+                          </table>
                         </div>
-                        {/* Item */}
                       </div>
-                    </div>
-                  </div>
-                </div>
-
-                {/* Item */}
-                <div className="col-4">
-                  <div className="card h-100">
-                    <div className="card-body">
-                      <h4 className="card-title mb-4 fw-bold">
-                        <i class="fas fa-play"></i> Sản phẩm bán chạy nhất
-                      </h4>
-                      {bestSellingProduct.map((p, index) => (
-                        <div className="row my-2" key={p.id}>
-                          <div className="col-12 text-truncate text-dark">
-                            <h6>
-                              {index + 1}. {p.name}
-                            </h6>
-                          </div>
-                        </div>
-                      ))}
+                      {/* Item */}
                     </div>
                   </div>
                 </div>
               </div>
+
+              {/* Item */}
+              <div className="col-4">
+                <div className="card h-100">
+                  <div className="card-body">
+                    <h4 className="card-title mb-4 fw-bold">
+                      <i class="fas fa-play"></i> Sản phẩm bán chạy nhất
+                    </h4>
+                    {bestSellingProduct.map((p, index) => (
+                      <div className="row my-2" key={p.id}>
+                        <div className="col-12 text-truncate text-dark">
+                          <h6>
+                            {index + 1}. {p.name}
+                          </h6>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
-          </>
-        )
-      }
+          </div>
+        </>
+      )}
     </>
   );
-}
+};
