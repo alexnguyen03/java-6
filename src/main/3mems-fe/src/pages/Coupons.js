05a34580ede79263869d29dbf7b6967cf963bb12
AlexNguyen <nguyenhoainam121n@gmail.com>
2023-08-09 14:02:54 +0700

    commit to presentation

--- src/main/3mems-fe/src/pages/Coupons.js
+++ src/main/3mems-fe/src/pages/Coupons.js
@@ -5,7 +5,13 @@ import moment from 'moment-timezone';
 import React, {useEffect, useState} from 'react';
 import DataTable from 'react-data-table-component';
 import Datetime from 'react-datetime';
-
+import {DateRangePicker} from '@mui/x-date-pickers-pro/DateRangePicker';
+import {format} from 'date-fns';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {SingleInputDateRangeField} from '@mui/x-date-pickers-pro/SingleInputDateRangeField';
+import {DemoContainer, DemoItem} from '@mui/x-date-pickers/internals/demo';
+import dayjs from 'dayjs';
 const ROOT_URL = 'http://localhost:8080/admin/coupons';
 
 export default () => {
@@ -21,16 +27,21 @@ export default () => {
 	const [showModal, setShowModal] = useState(false);
 	const [coupons, setCoupons] = useState([{}]);
 	const [update, setUpdate] = useState(false);
+	const [focusedInput, setFocusedInput] = useState(null);
 
 	//form
 	const [couponCode, setCouponCode] = useState('');
 	const [couponName, setCouponName] = useState('');
 	const [createdDate, setCreatedDate] = useState('');
 	const [discountAmount, setDiscountAmount] = useState(0);
-	const [startDate, setStartDate] = useState('');
+	const [startDate, setStartDate] = useState(new Date());
 	const [expirationDate, setExpirationDate] = useState('');
+	const [couponCodeError, setCouponCodeError] = useState('');
+	const [couponNameError, setCouponNameError] = useState('');
+	const [discountAmountError, setDiscountAmountError] = useState('');
 	const [activated, setActivated] = useState(true);
 	const [formValid, setFormValid] = useState(false);
+	const [rangeDate, setRangeDate] = useState([dayjs(new Date()), dayjs(new Date())]);
 
 	const [coupon, setCoupon] = useState({
 		couponCode: '',
@@ -111,40 +122,19 @@ export default () => {
 			console.log(error);
 		}
 	};
+
 	const handleGetCouponDetails = (row) => {
 		setUpdate(true);
 		setCouponCode(row.couponCode);
 		setCouponName(row.couponName);
 		setCreatedDate(row.createdDate);
 		setDiscountAmount(row.discountAmount);
-		setExpirationDate(row.expirationDate);
-		setStartDate(row.startDate);
 		setActivated(row.activated);
-		setShowCard(true);
+		console.log(activated);
+		setRangeDate([dayjs(row.startDate), dayjs(row.expirationDate)]);
 
-		// setShowAddBtn(false);
+		setShowCard(true);
 		setCoupon(row);
-		console.log(coupon);
-		// try {
-		// 	const resp = await fetch(ROOT_URL + `/users/${row.id}`);
-		// 	const data = await resp.json();
-		// 	setShowCard(true);
-		// 	setUser(data);
-		// 	setCoupon(row);
-		// } catch (error) {
-		// 	console.log(error);
-		// }
-		// try {
-		// 	const resp = await fetch(ROOT_URL + `/detail/${row.id}`);
-		// 	const od = await resp.json();
-		// } catch (error) {
-		// 	console.log(error);
-		// }
-		// if (row.status === 'H' || row.status === 'G') {
-		// 	setShowUpdateBtn(false);
-		// } else {
-		// 	setShowUpdateBtn(true);
-		// }
 	};
 	const displayStatus = (status) => {
 		return status ? 'Đang hoạt động' : 'Không hoạt động';
@@ -152,9 +142,7 @@ export default () => {
 
 	const handleUpdateCoupon = async (e) => {
 		// e.preventDefault();
-		Object.assign(coupon, {couponCode: couponCode, discountAmount: discountAmount, expirationDate: moment(expirationDate).format('yyyy-MM-DD'), startDate: moment(startDate).format('yyyy-MM-DD'), activated: activated, couponName: couponName, createdDate: moment(createdDate).format('yyyy-MM-DD')});
-
-		console.log(coupon);
+		Object.assign(coupon, {couponCode: couponCode, discountAmount: discountAmount, expirationDate: moment(rangeDate[1].$d).format('yyyy-MM-DD'), startDate: moment(rangeDate[0].$d).format('yyyy-MM-DD'), activated: activated, couponName: couponName, createdDate: moment(createdDate).format('yyyy-MM-DD')});
 		console.log(JSON.stringify(coupon));
 		try {
 			const resp = await fetch(ROOT_URL, {
@@ -174,11 +162,29 @@ export default () => {
 		getData();
 		console.log('updated');
 	};
-
+	const isFormValid = (coupon) => {
+		if (coupon.couponCode === '' || coupon.couponName === '' || coupon.discountAmount === 0) {
+			if (coupon.couponCode === '') {
+				setCouponCodeError('Vui lòng nhập vào Mã Khuyến Mãi !');
+			}
+			if (coupon.couponName === '') {
+				setCouponNameError('Vui lòng nhập vào Tên Khuyến Mãi !');
+			}
+			if (coupon.discountAmount === 0) {
+				setDiscountAmountError('Vui lòng nhập vào Phần trăm Khuyến Mãi !');
+			}
+			return false;
+		} else {
+			return true;
+		}
+	};
 	const handleAddCoupon = async () => {
-		Object.assign(coupon, {couponCode: couponCode, discountAmount: discountAmount, expirationDate: moment(expirationDate).format('yyyy-MM-DD'), startDate: moment(startDate).format('yyyy-MM-DD'), activated: activated, couponName: couponName, createdDate: moment(new Date()).format('yyyy-MM-DD')});
-		handleResetForm();
-		// console.log(coupon);
+		Object.assign(coupon, {couponCode: couponCode, discountAmount: discountAmount, expirationDate: moment(rangeDate[1].$d).format('yyyy-MM-DD'), startDate: moment(rangeDate[0].$d).format('yyyy-MM-DD'), activated: activated, couponName: couponName, createdDate: moment(new Date()).format('yyyy-MM-DD')});
+		console.log(JSON.stringify(coupon));
+		// console.log();
+		if (!isFormValid(coupon)) {
+			return;
+		}
 		try {
 			const resp = await fetch(ROOT_URL, {
 				method: 'POST',
@@ -189,10 +195,12 @@ export default () => {
 				body: JSON.stringify(coupon),
 			});
 			const data = resp.json();
-			getData();
+			// getData();
+			// setCoupons(...coupons, data);
 		} catch (error) {
 			console.log(error);
 		}
+		handleResetForm();
 		setShowToast(true);
 	};
 	const handleResetForm = () => {
@@ -202,38 +210,14 @@ export default () => {
 		setStartDate('');
 		setDiscountAmount('');
 		setExpirationDate('');
+		setActivated(true);
+		setRangeDate([dayjs(new Date()), dayjs(new Date())]);
 	};
 
-	const handleSubmitForm = async (e) => {
-		e.preventDefault();
-		handleCloseModal();
-		setShowNote(false);
-		setShowUpdateBtn(false);
-		Object.assign(coupon, {status: newStatus});
-		if (notes !== '') {
-			Object.assign(coupon, {notes: notes});
-		}
-		console.log(coupon);
-
-		try {
-			const resp = await fetch(ROOT_URL + `/${coupon.id}`, {
-				method: 'PUT',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(coupon),
-			});
-			const data = resp.json();
-			setNotes('');
-		} catch (error) {
-			console.log(error);
-		}
-		setShowToast(true);
-	};
 	const handleOnChangeSelect = (e) => {
 		setActivated(e.target.value);
 	};
+
 	useEffect(() => {
 		getData();
 	}, []);
@@ -291,7 +275,7 @@ export default () => {
 														placeholder='Mã khuyến mãi'
 													/>
 												</Form.Group>
-												{/* {errors.couponCode && <p className='alert alert-warning'>Vui lòng không để trống mã</p>} */}
+												{couponCodeError && <i className='text-danger'>{couponCodeError}</i>}
 											</Col>
 											<Col
 												md={6}
@@ -306,6 +290,7 @@ export default () => {
 														placeholder='Tên khuyến mãi'
 													/>
 												</Form.Group>
+												{couponNameError && <i className='text-danger'>{couponNameError}</i>}
 											</Col>
 										</Row>
 										<Row>
@@ -325,6 +310,7 @@ export default () => {
 														placeholder='Phần trăm giảm giá'
 													/>
 												</Form.Group>
+												{discountAmountError && <i className='text-danger'>{discountAmountError}</i>}
 											</Col>
 											<Col
 												md={6}
@@ -340,59 +326,18 @@ export default () => {
 												</Form.Group>
 											</Col>
 										</Row>
-										<Row className='align-items-center'>
-											<Col
-												md={6}
-												className='mb-3'>
-												<Form.Group id='startDate'>
-													<Form.Label>Ngày bắt đầu</Form.Label>
-													<Datetime
-														timeFormat={false}
-														onChange={setStartDate}
-														renderInput={(props, openCalendar) => (
-															<InputGroup>
-																<InputGroup.Text>
-																	<FontAwesomeIcon icon={faCalendarAlt} />
-																</InputGroup.Text>
-																<Form.Control
-																	required
-																	type='text'
-																	value={startDate ? moment(startDate).format('MM/DD/YYYY') : ''}
-																	placeholder='MM/DD/YYYY'
-																	onFocus={openCalendar}
-																	onChange={() => {}}
-																/>
-															</InputGroup>
-														)}
+										<Row>
+											<LocalizationProvider dateAdapter={AdapterDayjs}>
+												<DemoItem
+													label='Ngày bắt đầu - Ngày kết thúc'
+													component='DateRangePicker'>
+													<DateRangePicker
+														value={rangeDate}
+														// onChange={(newValue) => handleDateChange(newValue)}
+														onChange={(newValue) => setRangeDate(newValue)}
 													/>
-												</Form.Group>
-											</Col>
-											<Col
-												md={6}
-												className='mb-3'>
-												<Form.Group id='startDate'>
-													<Form.Label>Ngày kết thúc</Form.Label>
-													<Datetime
-														timeFormat={false}
-														onChange={setExpirationDate}
-														renderInput={(props, openCalendar) => (
-															<InputGroup>
-																<InputGroup.Text>
-																	<FontAwesomeIcon icon={faCalendarAlt} />
-																</InputGroup.Text>
-																<Form.Control
-																	required
-																	type='text'
-																	value={expirationDate ? moment(expirationDate).format('MM/DD/YYYY') : ''}
-																	placeholder='MM/DD/YYYY'
-																	onFocus={openCalendar}
-																	onChange={() => {}}
-																/>
-															</InputGroup>
-														)}
-													/>
-												</Form.Group>
-											</Col>
+												</DemoItem>
+											</LocalizationProvider>
 										</Row>
 
 										<div className='mt-3'>
