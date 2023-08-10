67127d36eae2372e3a378f49323d0b2c3f5c08b7
leminhduong2003 <minhdeluong2003@gmail.com>
2023-08-10 13:44:52 +0700

    Quan-ly-danh-muc

--- src/main/3mems-fe/src/pages/Coupons.js
+++ src/main/3mems-fe/src/pages/Coupons.js
@@ -1,14 +1,13 @@
 import {faCalendarAlt, faPenSquare} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {Button, Card, Col, Form, InputGroup, Row, Toast} from '@themesberg/react-bootstrap';
-import moment from 'moment-timezone';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import React, {useEffect, useState} from 'react';
 import DataTable from 'react-data-table-component';
-import Datetime from 'react-datetime';
-
-const ROOT_URL = 'http://localhost:8080/admin/coupons';
+const ROOT_URL = 'http://localhost:8080/admin/categorys';
 
 export default () => {
+	const [toastMessage, setToastMessage] = useState('');
 	const [showCard, setShowCard] = useState(false);
 	const [showAddBtn, setShowAddBtn] = useState(true);
 	const [showToast, setShowToast] = useState(false);
@@ -19,63 +18,28 @@ export default () => {
 	const [newStatus, setNewStatus] = useState('');
 	const [notes, setNotes] = useState('');
 	const [showModal, setShowModal] = useState(false);
-	const [coupons, setCoupons] = useState([{}]);
+	const [categorys, setCategorys] = useState([{}]);
 	const [update, setUpdate] = useState(false);
 
 	//form
-	const [couponCode, setCouponCode] = useState('');
-	const [couponName, setCouponName] = useState('');
-	const [createdDate, setCreatedDate] = useState('');
-	const [discountAmount, setDiscountAmount] = useState(0);
-	const [startDate, setStartDate] = useState('');
-	const [expirationDate, setExpirationDate] = useState('');
-	const [activated, setActivated] = useState(true);
-	const [formValid, setFormValid] = useState(false);
+	const [id, setId] = useState('');
+	const [name, setName] = useState('');
+	const [errors, setErrors] = useState({});
 
-	const [coupon, setCoupon] = useState({
-		couponCode: '',
-		discountAmount: 0,
-		expirationDate: '',
-		startDate: '',
-		activated: true,
-		couponName: '',
-		createdDate: '',
+	const [category, setCategory] = useState({
+		id: '',
+		name: '',
 	});
 
 	const columns = [
 		{
-			name: 'Mã giảm giá',
-			selector: (row) => row.couponCode,
-		},
-		{
-			name: 'Tên khuyến mãi',
-			selector: (row) => row.couponName,
+			name: 'Mã danh mục',
+			selector: (row) => row.id,
 			sortable: true,
 		},
 		{
-			name: 'Phần trăm giảm',
-			sortable: true,
-			selector: (row) => row.discountAmount,
-		},
-
-		{
-			name: 'Ngày tạo',
-			selector: (row) => row.createdDate,
-			sortable: true,
-		},
-		{
-			name: 'Ngày bắt đầu',
-			selector: (row) => row.startDate,
-			sortable: true,
-		},
-		{
-			name: 'Ngày hết hạn',
-			selector: (row) => row.expirationDate,
-			sortable: true,
-		},
-		{
-			name: 'Trạng thái',
-			selector: (row) => displayStatus(row.activated),
+			name: 'Tên danh mục',
+			selector: (row) => row.name,
 			sortable: true,
 		},
 		{
@@ -105,26 +69,61 @@ export default () => {
 			{
 				setLoading(false);
 				console.log(data);
-				setCoupons(data);
+				setCategorys(data);
+				setRecords(data);
 			}
 		} catch (error) {
 			console.log(error);
 		}
 	};
+
+	const validateForm = () => {
+		let formIsValid = true;
+		const newErrors = {};
+	  
+		if (!id) {
+		  newErrors.id = 'Vui lòng nhập mã danh mục';
+		  formIsValid = false;
+		}
+	  
+		if (!name) {
+		  newErrors.name = 'Vui lòng nhập tên danh mục';
+		  formIsValid = false;
+		}
+	  
+		setErrors(newErrors);
+		return formIsValid;
+	};
+
+	const conditionalRowStyles = [
+		{
+		  when: row => row.id === category.id,
+		  style: row => ({ backgroundColor: 'rgb(109, 235, 198)'}),
+		},
+	  ];
+	
+	const [records, setRecords] = useState(categorys);
+
+	function handleFilter(event) {
+		const newData = categorys.filter(row => {
+			return row.name.toLowerCase().includes(event.target.value.toLowerCase())
+		})
+		if (newData.length === 0) {
+			setToastMessage('Không tìm thấy danh mục!');
+			setShowToast(true);
+		  }
+		setRecords(newData);
+	};
+
 	const handleGetCouponDetails = (row) => {
 		setUpdate(true);
-		setCouponCode(row.couponCode);
-		setCouponName(row.couponName);
-		setCreatedDate(row.createdDate);
-		setDiscountAmount(row.discountAmount);
-		setExpirationDate(row.expirationDate);
-		setStartDate(row.startDate);
-		setActivated(row.activated);
+		setId(row.id);
+		setName(row.name);
 		setShowCard(true);
 
 		// setShowAddBtn(false);
-		setCoupon(row);
-		console.log(coupon);
+		setCategory(row);
+		console.log(category);
 		// try {
 		// 	const resp = await fetch(ROOT_URL + `/users/${row.id}`);
 		// 	const data = await resp.json();
@@ -146,16 +145,14 @@ export default () => {
 		// 	setShowUpdateBtn(true);
 		// }
 	};
-	const displayStatus = (status) => {
-		return status ? 'Đang hoạt động' : 'Không hoạt động';
-	};
+
 
 	const handleUpdateCoupon = async (e) => {
 		// e.preventDefault();
-		Object.assign(coupon, {couponCode: couponCode, discountAmount: discountAmount, expirationDate: moment(expirationDate).format('yyyy-MM-DD'), startDate: moment(startDate).format('yyyy-MM-DD'), activated: activated, couponName: couponName, createdDate: moment(createdDate).format('yyyy-MM-DD')});
+		Object.assign(category, {id: id, name: name});
 
-		console.log(coupon);
-		console.log(JSON.stringify(coupon));
+		console.log(category);
+		console.log(JSON.stringify(category));
 		try {
 			const resp = await fetch(ROOT_URL, {
 				method: 'PUT',
@@ -163,11 +160,10 @@ export default () => {
 					Accept: 'application/json',
 					'Content-Type': 'application/json',
 				},
-				body: JSON.stringify(coupon),
+				body: JSON.stringify(category),
 			});
 			const data = resp.json();
-			setCoupon(data);
-			console.log(coupon.activated);
+			setCategory(data);
 		} catch (error) {
 			console.log(error);
 		}
@@ -176,64 +172,72 @@ export default () => {
 	};
 
 	const handleAddCoupon = async () => {
-		Object.assign(coupon, {couponCode: couponCode, discountAmount: discountAmount, expirationDate: moment(expirationDate).format('yyyy-MM-DD'), startDate: moment(startDate).format('yyyy-MM-DD'), activated: activated, couponName: couponName, createdDate: moment(new Date()).format('yyyy-MM-DD')});
-		handleResetForm();
-		// console.log(coupon);
-		try {
-			const resp = await fetch(ROOT_URL, {
-				method: 'POST',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(coupon),
-			});
-			const data = resp.json();
-			getData();
-		} catch (error) {
-			console.log(error);
-		}
-		setShowToast(true);
+			Object.assign(category, {id: id, name: name});
+			handleResetForm();
+			try {
+				const resp = await fetch(ROOT_URL, {
+					method: 'POST',
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(category),
+				});
+				const data = resp.json();
+				getData();
+			} catch (error) {
+				console.log(error);
+			}
+
 	};
 	const handleResetForm = () => {
 		setUpdate(false);
-		setCouponCode('');
-		setCouponName('');
-		setStartDate('');
-		setDiscountAmount('');
-		setExpirationDate('');
+		setId('');
+		setName('');
 	};
 
-	const handleSubmitForm = async (e) => {
-		e.preventDefault();
-		handleCloseModal();
-		setShowNote(false);
-		setShowUpdateBtn(false);
-		Object.assign(coupon, {status: newStatus});
-		if (notes !== '') {
-			Object.assign(coupon, {notes: notes});
-		}
-		console.log(coupon);
+	// const handleSubmitForm = async (e) => {
+	// 	e.preventDefault();
+	// 	handleCloseModal();
+	// 	setShowNote(false);
+	// 	setShowUpdateBtn(false);
+	// 	Object.assign(category, {status: newStatus});
+	// 	if (notes !== '') {
+	// 		Object.assign(category, {notes: notes});
+	// 	}
+	// 	console.log(category);
 
-		try {
-			const resp = await fetch(ROOT_URL + `/${coupon.id}`, {
-				method: 'PUT',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(coupon),
-			});
-			const data = resp.json();
-			setNotes('');
-		} catch (error) {
-			console.log(error);
+	// 	try {
+	// 		const resp = await fetch(ROOT_URL + `/${category.id}`, {
+	// 			method: 'PUT',
+	// 			headers: {
+	// 				Accept: 'application/json',
+	// 				'Content-Type': 'application/json',
+	// 			},
+	// 			body: JSON.stringify(category),
+	// 		});
+	// 		const data = resp.json();
+	// 		setNotes('');
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// 	setShowToast(true);
+	// };
+
+	const handleAdd = () => {
+		if (validateForm()) {
+			setToastMessage('Thêm danh mục thành công!');
+			setShowToast(true);
+		  }
+	  };
+	
+	const handleUpdate = () => {
+		if (validateForm()) {
+			setToastMessage('Cập nhật danh mục thành công!');
+			setShowToast(true);
 		}
-		setShowToast(true);
 	};
-	const handleOnChangeSelect = (e) => {
-		setActivated(e.target.value);
-	};
+
 	useEffect(() => {
 		getData();
 	}, []);
@@ -242,16 +246,12 @@ export default () => {
 		<>
 			<Row className='mb-3'>
 				<Row className='my-3 position-relative'>
-					<Toast
-						onClose={() => setShowToast(false)}
-						show={showToast}
-						autohide
-						className='position-absolute top-0 start-50 translate-middle-x z-3 bg-success'>
-						<Toast.Header>
-							<strong className='me-auto'>4MEMS - Thông báo</strong>
-						</Toast.Header>
-						<Toast.Body className='text-white'>Cập nhật trạng thái đơn hàng thành công !</Toast.Body>
-					</Toast>
+				<Toast onClose={() => setShowToast(false)} show={showToast} autohide className='position-absolute top-0 start-50 translate-middle-x z-3 bg-success'>
+					<Toast.Header>
+					<strong className="me-auto">4MEMS - Thông báo</strong>
+					</Toast.Header>
+					<Toast.Body className="text-white">{toastMessage}</Toast.Body>
+				</Toast>
 					{showAddBtn && (
 						<Col xl={12}>
 							<Button
@@ -261,8 +261,20 @@ export default () => {
 									setShowAddBtn(false);
 								}}
 								variant='success'>
-								Thêm khuyến mãi
+								Thêm danh mục
 							</Button>
+							<Col xl={4}>
+								<InputGroup className='input-group-merge search-bar'>
+										<InputGroup.Text>
+											<FontAwesomeIcon icon={faSearch} />
+										</InputGroup.Text>
+										<Form.Control
+											type='text'
+											placeholder='Tìm theo tên danh mục'
+											onChange={handleFilter}
+										/>
+								</InputGroup>
+							</Col>
 						</Col>
 					)}
 				</Row>
@@ -273,23 +285,23 @@ export default () => {
 								border='light'
 								className='bg-white shadow-sm mb-4'>
 								<Card.Body>
-									<h5 className='mb-4'>Thông tin khuyến mãi</h5>
+									<h5 className='mb-4'>Quản lý danh mục</h5>
 									<Form>
 										<Row>
 											<Col
 												md={6}
 												className='mb-3'>
 												<Form.Group id='firstName'>
-													<Form.Label>Mã khuyến mãi</Form.Label>
+													<Form.Label>Mã danh mục</Form.Label>
 													<Form.Control
-														onChange={(e) => setCouponCode(e.target.value)}
-														required
-														value={couponCode}
+														onChange={(e) => setId(e.target.value)}
+														value={id}
 														maxLength={6}
 														readOnly={update}
 														type='text'
-														placeholder='Mã khuyến mãi'
+														placeholder='Mã danh mục'
 													/>
+													{errors.id && <p className='alert alert-warning'>{errors.id}</p>}
 												</Form.Group>
 												{/* {errors.couponCode && <p className='alert alert-warning'>Vui lòng không để trống mã</p>} */}
 											</Col>
@@ -297,107 +309,38 @@ export default () => {
 												md={6}
 												className='mb-3'>
 												<Form.Group id='lastName'>
-													<Form.Label>Tên khuyến mãi</Form.Label>
+													<Form.Label>Tên danh mục</Form.Label>
 													<Form.Control
-														onChange={(e) => setCouponName(e.target.value)}
-														required
-														value={couponName}
+														onChange={(e) => setName(e.target.value)}														value={name}
 														type='text'
-														placeholder='Tên khuyến mãi'
+														placeholder='Tên danh mục'
 													/>
-												</Form.Group>
-											</Col>
-										</Row>
-										<Row>
-											<Col
-												md={6}
-												className='mb-3'>
-												<Form.Group id='firstName'>
-													<Form.Label>Phần trăm giảm giá</Form.Label>
-													<Form.Control
-														onChange={(e) => setDiscountAmount(e.target.value)}
-														required
-														value={discountAmount}
-														type='number'
-														min={0}
-														max={100}
-														step={10}
-														placeholder='Phần trăm giảm giá'
-													/>
-												</Form.Group>
-											</Col>
-											<Col
-												md={6}
-												className='mb-3'>
-												<Form.Group id='gender'>
-													<Form.Label>Trạng thái</Form.Label>
-													<Form.Select
-														defaultValue={coupon.activated}
-														onChange={handleOnChangeSelect}>
-														<option value='true'>Đang hoạt động</option>
-														<option value='false'>Không hoạt động</option>
-													</Form.Select>
-												</Form.Group>
-											</Col>
-										</Row>
-										<Row className='align-items-center'>
-											<Col
-												md={6}
-												className='mb-3'>
-												<Form.Group id='startDate'>
-													<Form.Label>Ngày bắt đầu</Form.Label>
-													<Datetime
-														timeFormat={false}
-														onChange={setStartDate}
-														renderInput={(props, openCalendar) => (
-															<InputGroup>
-																<InputGroup.Text>
-																	<FontAwesomeIcon icon={faCalendarAlt} />
-																</InputGroup.Text>
-																<Form.Control
-																	required
-																	type='text'
-																	value={startDate ? moment(startDate).format('MM/DD/YYYY') : ''}
-																	placeholder='MM/DD/YYYY'
-																	onFocus={openCalendar}
-																	onChange={() => {}}
-																/>
-															</InputGroup>
-														)}
-													/>
-												</Form.Group>
-											</Col>
-											<Col
-												md={6}
-												className='mb-3'>
-												<Form.Group id='startDate'>
-													<Form.Label>Ngày kết thúc</Form.Label>
-													<Datetime
-														timeFormat={false}
-														onChange={setExpirationDate}
-														renderInput={(props, openCalendar) => (
-															<InputGroup>
-																<InputGroup.Text>
-																	<FontAwesomeIcon icon={faCalendarAlt} />
-																</InputGroup.Text>
-																<Form.Control
-																	required
-																	type='text'
-																	value={expirationDate ? moment(expirationDate).format('MM/DD/YYYY') : ''}
-																	placeholder='MM/DD/YYYY'
-																	onFocus={openCalendar}
-																	onChange={() => {}}
-																/>
-															</InputGroup>
-														)}
-													/>
+													{errors.name && <p className='alert alert-warning'>{errors.name}</p>}
 												</Form.Group>
 											</Col>
 										</Row>
 
 										<div className='mt-3'>
+										<Button
+												className='mx-2'
+												onClick={() => {
+													handleAdd();
+													handleAddCoupon();
+												  }}
+												variant='success'
+												disabled={update}
+
+												// type='submit'
+											>
+												Thêm
+											</Button>
+
 											<Button
-												onClick={() => handleUpdateCoupon()}
+											className='mx-2'
+											onClick={() => {
+												handleUpdate();
+												handleUpdateCoupon();
+											  }}
 												variant='info'
 												disabled={!update}
 												// type='submit'
@@ -405,16 +348,6 @@ export default () => {
 												Cập nhật
 											</Button>
 
-											<Button
-												className='mx-2'
-												onClick={() => handleAddCoupon()}
-												variant='success'
-												disabled={update}
-
-												// type='submit'
-											>
-												Thêm khuyến mãi
-											</Button>
 											<Button
 												className=''
 												onClick={() => handleResetForm()}
@@ -436,12 +369,13 @@ export default () => {
 					<DataTable
 						// expandableRows
 						columns={columns}
-						data={coupons}
+						data={records}
 						pagination
 						highlightOnHover
 						progressPending={loading}
 						fixedHeader
 						fixedHeaderScrollHeight='500px'
+						conditionalRowStyles={conditionalRowStyles}
 					/>
 				</Col>
 			</Row>
